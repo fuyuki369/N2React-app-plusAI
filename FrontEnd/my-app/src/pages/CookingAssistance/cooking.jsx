3e7635a8fc2adc62ada383e336cc9694d8372071
トライアルA <unicast.trial.a@unicast.ne.jp>
2025-10-30 19:58:49 +0900

    コミット

--- FrontEnd/my-app/src/pages/CookingAssistance/cooking.jsx
+++ FrontEnd/my-app/src/pages/CookingAssistance/cooking.jsx
@@ -52,115 +52,126 @@ function CookingAssistance(){
                         {/*食材*/}  {/*inputやselectのvalueは一旦後*/}
                         <div className="ingredient">
                             <h4>食材・材料</h4>
-                            <label htmlFor="mein-ingredient">主な食材</label>  {/*reactではforはhtmlForになる*/}
-                            <input 
-                                type="text"
-                                id="mein-ingredient"
-                                value={meinIngredient}
-                                onChange={(e) =>setMeinIngredient(e)}   //valueとonChangeはセット意識
-                            />
-                            <label htmlFor="sub-ingredient">追加したい食材</label>
-                            <input 
-                                type="text" 
-                                id="sub-ingredient"
-                                value={subIngredient}
-                                onChange={(e) =>setSubIngredient(e)}
-                            />
-                            <label htmlFor="avoid-ingredient">避けたい食材</label>
-                            <input 
-                                type="text"
-                                id="avoid-ingredient"
-                                value={avoidIngredient}
-                                onChange={(e) =>setAvoidIngredient(e)}
-                            />
+                            <div className="selection ingredient-selection">
+                                <label htmlFor="mein-ingredient">主な食材</label>  {/*reactではforはhtmlForになる*/}
+                                <input 
+                                    type="text"
+                                    id="mein-ingredient"
+                                    value={meinIngredient}
+                                    onChange={(e) =>setMeinIngredient(e)}   //valueとonChangeはセット意識
+                                />
+                                <label htmlFor="sub-ingredient">追加したい食材</label>
+                                <input 
+                                    type="text" 
+                                    id="sub-ingredient"
+                                    value={subIngredient}
+                                    onChange={(e) =>setSubIngredient(e)}
+                                />
+                                <label htmlFor="avoid-ingredient">避けたい食材</label>
+                                <input 
+                                    type="text"
+                                    id="avoid-ingredient"
+                                    value={avoidIngredient}
+                                    onChange={(e) =>setAvoidIngredient(e)}
+                                />
+                            </div>
                         </div>
                         {/*ジャンル・分類*/}
                         <div className="genre">
                             <h4>ジャンル・分類</h4>
-                            <label htmlFor="cuisine-type">料理ジャンル</label> 
-                            <select id="cuisine-type" value={cuisineType} onChange={(e) =>setCuisineType(e)}>
-                                <option value="">指定無し</option>
-                                <option value="japanese-cuisine">和食</option>
-                                <option value="western-cuisine">洋食</option>
-                                <option value="chinese-cuisine">中華</option>
-                                <option value="italian-cuisine">イタリアン</option>
-                                <option value="korean-cuisine">韓国料理</option>
-                            </select>
-                            <label htmlFor="category">分類</label> 
-                            <select id="category" value={category} onChange={(e) =>setCategory(e)}>
-                                <option value="">指定無し</option>
-                                <option value="main-dish">主菜</option>
-                                <option value="side-dish">副菜</option>
-                                <option value="soup">スープ</option>
-                                <option value="dessert">デザート</option>
-                                <option value="drink">ドリンク</option>
-                            </select>
-                            <label htmlFor="season">シーズン</label> 
-                            <select id="season" value={season} onChange={(e) =>setSeason(e)}>
-                                <option value="">指定無し</option>
-                                <option value="spring">春</option>
-                                <option value="summer">夏</option>
-                                <option value="fall">秋</option>
-                                <option value="winter">冬</option>
-                            </select>
+                            <div className="selection genre-selection">
+                                <label htmlFor="cuisine-type">料理ジャンル</label> 
+                                <select id="cuisine-type" value={cuisineType} onChange={(e) =>setCuisineType(e)}>
+                                    <option value="">指定無し</option>
+                                    <option value="japanese-cuisine">和食</option>
+                                    <option value="western-cuisine">洋食</option>
+                                    <option value="chinese-cuisine">中華</option>
+                                    <option value="italian-cuisine">イタリアン</option>
+                                    <option value="korean-cuisine">韓国料理</option>
+                                </select>
+                                <label htmlFor="category">分類</label> 
+                                <select id="category" value={category} onChange={(e) =>setCategory(e)}>
+                                    <option value="">指定無し</option>
+                                    <option value="main-dish">主菜</option>
+                                    <option value="side-dish">副菜</option>
+                                    <option value="soup">スープ</option>
+                                    <option value="dessert">デザート</option>
+                                    <option value="drink">ドリンク</option>
+                                </select>
+                                <label htmlFor="season">シーズン</label> 
+                                <select id="season" value={season} onChange={(e) =>setSeason(e)}>
+                                    <option value="">指定無し</option>
+                                    <option value="spring">春</option>
+                                    <option value="summer">夏</option>
+                                    <option value="fall">秋</option>
+                                    <option value="winter">冬</option>
+                                </select>
+                            </div>
                         </div>
                         {/*時間・手間*/}
                         <div className="effort">
                             <h4>調理時間・難易度</h4>
-                            <label htmlFor="cook-time">調理時間</label>
-                            <select id="cook-time" value={cookTime} onChange={(e) =>setCookTime(e)}>
-                                <option value="">指定無し</option>
-                                <option value="5min">5分以内</option>
-                                <option value="15min">15分以内</option>
-                                <option value="30min">30分以内</option>
-                                <option value="60min">1時間以内</option>
-                            </select>
-                            <label htmlFor="difficulty">難易度</label>
-                            <select id="difficulty" value={difficulty} onChange={(e) =>setDifficulty(e)}>
-                                <option value="">指定無し</option>
-                                <option value="easy">簡単</option>
-                                <option value="medium">普通</option>
-                                <option value="hard">手の込んだ料理</option>
-                            </select>
-                            <label htmlFor="cost">予算</label>
-                            <select id="const" value={cost} onChange={(e) =>setCost(e)}>
-                                <option value="">指定無し</option>
-                                <option value="500yen">~500円</option>
-                                <option value="1000yen">~1000円</option>
-                                <option value="3000yen">~3000円</option>
-                                <option value="5000yen">~5000円</option>
-                                <option value="10,000yen">~1万円</option>
-                            </select>
+                            <div className="selection effort-selection">
+                                <label htmlFor="cook-time">調理時間</label>
+                                <select id="cook-time" value={cookTime} onChange={(e) =>setCookTime(e)}>
+                                    <option value="">指定無し</option>
+                                    <option value="5min">5分以内</option>
+                                    <option value="15min">15分以内</option>
+                                    <option value="30min">30分以内</option>
+                                    <option value="60min">1時間以内</option>
+                                </select>
+                                <label htmlFor="difficulty">難易度</label>
+                                <select id="difficulty" value={difficulty} onChange={(e) =>setDifficulty(e)}>
+                                    <option value="">指定無し</option>
+                                    <option value="easy">簡単</option>
+                                    <option value="medium">普通</option>
+                                    <option value="hard">手の込んだ料理</option>
+                                </select>
+                                <label htmlFor="cost">予算</label>
+                                <select id="const" value={cost} onChange={(e) =>setCost(e)}>
+                                    <option value="">指定無し</option>
+                                    <option value="500yen">~500円</option>
+                                    <option value="1000yen">~1000円</option>
+                                    <option value="3000yen">~3000円</option>
+                                    <option value="5000yen">~5000円</option>
+                                    <option value="10,000yen">~1万円</option>
+                                </select>
+                            </div>
                         </div>
                         {/*シチュエーション*/}
                         <div className="situation">
                             <h4>シチュエーション</h4>
-                            <label htmlFor="whom">誰と食べる？</label>
-                            <select id="whom" value={whom} onChange={(e) =>setWhom(e)}>
-                                <option value="">指定無し</option>
-                                <option value="solo">１人</option>
-                                <option value="friends">友達</option>
-                                <option value="partner">恋人</option>
-                                <option value="family">家族</option>
-                            </select>
-                            <label htmlFor="mood">どんな気分？</label>
-                            <input 
-                                type="text"
-                                placeholder="疲れている/元気出したい/リラックス"
-                                value={mood}
-                                onChange={(e) =>setMood(e)}
-                            />
+                            <div className="selection effort-selection">
+                                <label htmlFor="whom">誰と食べる？</label>
+                                <select id="whom" value={whom} onChange={(e) =>setWhom(e)}>
+                                    <option value="">指定無し</option>
+                                    <option value="solo">１人</option>
+                                    <option value="friends">友達</option>
+                                    <option value="partner">恋人</option>
+                                    <option value="family">家族</option>
+                                </select>
+                                <label htmlFor="mood">どんな気分？</label>
+                                <input 
+                                    type="text"
+                                    placeholder="疲れている/元気出したい/リラックス"
+                                    value={mood}
+                                    onChange={(e) =>setMood(e)}
+                                />
+                            </div>
                         </div>
                         {/*テイスト・雰囲気・目的*/}
                         <div className="taste">
                             <h4>テイスト・栄養・雰囲気を自由に</h4>
-                            <input 
-                                type="text"
-                                placeholder="おしゃれ/家庭的/さっぱり/濃い味/ダイエット"
-                                value={taste}
-                                onChange={(e) =>setTaste(e)}
-                            />
+                            <div className="selection taste-selection">
+                                <input 
+                                    type="text"
+                                    placeholder="おしゃれ/家庭的/さっぱり/濃い味/ダイエット"
+                                    value={taste}
+                                    onChange={(e) =>setTaste(e)}
+                                />
+                            </div>
                         </div>
+                        {/*送信ボタン*/}
                     </div>
                 </section>
                 {/*レシピ等提案部分*/}
