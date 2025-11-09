5a2a822103b4e7b3debaaa3b12607aa44c964b4d
yuki <unicast.trial.a@unicast.ne.jp>
2025-11-09 13:49:35 +0900

    ルーター書き始め・main.mjs修正

---
+++ BackEnd/project/CookingProject/Cooking_routes/cookingRoutes.mjs
@@ -0,0 +1,15 @@
+/*ルーター*/
+
+import express from 'express';
+
+//ファイルからインポートする部分  //import { 取得する値 } from "ファイルの場所"
+
+
+//ルーター作成
+const router = express.Router();
+
+//ルーターエリア //router.post("指定URL",インポートした値)
+
+
+//エクスポート
+export default router;
