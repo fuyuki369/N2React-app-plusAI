4a42778d86367bc4d14d73f0d6e85951051fccad
yuki <unicast.trial.a@unicast.ne.jp>
2025-11-12 13:23:29 +0900

    add: .envファイルを追加し.gitignoreに管理を追加 add: dotenvをインストール undata: main.mjsのdotenvのインポートとコンソール確認

--- BackEnd/project/main.mjs
+++ BackEnd/project/main.mjs
@@ -1,5 +1,7 @@
 /*サーバーエントリーポイント*/
 
+import 'dotenv/config';  //環境変数取得
+
 import express from "express";      //npm install express インストール確認済み
 import cors from "cors";            //npm install cors    インストール確認済み
 
@@ -21,6 +23,9 @@ app.use(express.json());
 
 //ルートからの機能を使用するコード app.use("ルートURL",ルートインポート値)
 app.use("/api/cooking",cookingRoutes);  //料理AIアシスタント(CookingAssistance)
+
+//環境変数確認  //確認済み
+console.log("環境変数:", process.env.OPENAI_API_KEY);
 
 //サーバー起動  //cd BackEnd → node project/main.mjs で起動
 app.listen(PORT, () => {
